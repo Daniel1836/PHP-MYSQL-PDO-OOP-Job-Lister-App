b5e17ea0b78e10354abbf313b02cd55feaf1e5f5
Daniel Klein <daniel.klein@videotron.ca>
2020-12-09 11:02:01 -0500

    Update frontpage.php

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -10,19 +10,19 @@
                 <?php endforeach; ?>
 		    </select>
 		    <br>
-		<input type="submit" class="btn btn-lg btn-success" style="background-color: #8EC5FC;" value="FIND">
+		<input type="submit" class="btn btn-lg btn-success" value="FIND">
 	    </form>
 </div>
-   <h3 style="color:grey;"><?php echo $title; ?> </h3>
+   <h3><?php echo $title; ?> </h3>
 <?php foreach($jobs as $job):?>
 
 <div class="row marketing">
 	<div class="col-md-10">
-		<h4 style="color:grey;"><?php echo $job->job_title;?></h4>
-		<p style="color:grey;"><?php echo $job->description;?></p>
+		<h4><?php echo $job->job_title;?></h4>
+		<p><?php echo $job->description;?></p>
 	</div>
     <div class="col-md-2">
-	<a class="btn btn-default" style="background-color: #acafb3;" href="job.php?id=<?php echo $job->id;?>">View</a>
+	<a class="btn btn-default" href="job.php?id=<?php echo $job->id;?>">View</a>
     </div>
 </div>
 <?php endforeach;?>
