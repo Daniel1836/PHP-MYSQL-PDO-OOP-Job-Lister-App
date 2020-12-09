b70aa4dfb42503da10b2bbede334394745ccbc4e
Daniel Klein <daniel.klein@videotron.ca>
2020-12-09 10:39:06 -0500

    Add files via upload

---
+++ init.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+require_once 'config.php';
+
+require_once 'helpers/system_helper.php';
+
+function __autoload($class_name)
+{
+require_once 'lib/'.$class_name. '.php';
+}
+
